bfbc74ee6012964d716380f24af978c92dd7451d
Sam <samuelgardner101@gmail.com>
2024-04-01 16:33:43 +0100

    Launch in new terminal

--- src/ClientConnectionManager.py
+++ src/ClientConnectionManager.py
@@ -360,7 +360,7 @@ class ClientConnectionManager(ConnectionManager):
         # Create the message window (as a command line window), and save the process.
         port = 20003 + len(self._chat_info)
         encoded_recipient_id = b64encode(recipient_id).decode()
-        process = subprocess.Popen(f"python src/ClientMessagingShell.py {port} {encoded_recipient_id}", shell=True)
+        process = subprocess.Popen(args=["python", "src/ClientMessagingShell.py", port, encoded_recipient_id], shell=True)
         self._chat_info[recipient_id].process = process
 
     def _handle_error(self, address: IPv6Address, data: bytes) -> None:
