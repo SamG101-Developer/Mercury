4dad5caec545b336a9e17c451c30ac18bd890094
Sam <samuelgardner101@gmail.com>
2024-04-02 16:54:48 +0100

    Debug

--- src/ClientConnectionManager.py
+++ src/ClientConnectionManager.py
@@ -269,6 +269,8 @@ class ClientConnectionManager(ConnectionManager):
         self._server_socket.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, multicast_request)
 
     def _handle_group_chat_invite(self, addr: IPv6Address, data: bytes) -> None:
+        print(f"invited to a group chat: {data[IP_SIZE:]}")
+
         # Extract the multicast address and group id from the data.
         multicast_address = IPv6Address(data[:IP_SIZE])
         group_id = data[IP_SIZE:IP_SIZE + DIGEST_SIZE]
